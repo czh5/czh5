9873267cfe88f42d24a4ddcfd5a20109bf864aca
czh <2865455975@qq.com>
2021-09-18 12:59:54 +0800

    第二版主类

--- 个人项目/CheckRepeat/src/main/Application.java
+++ 个人项目/CheckRepeat/src/main/Application.java
@@ -6,9 +6,9 @@ import util.WordCut;
 
 public class Application {
     public static void main(String[] args) {
-        String origPath = args[0];
-        String compPath = args[1];
-        String resultPath = args[2];
+        String origPath = args[0];  //原文路径
+        String compPath = args[1];  //抄袭文本路径
+        String resultPath = args[2];    //结果文件路径
 
         long start = System.currentTimeMillis();    //开始时间
 
@@ -19,4 +19,5 @@ public class Application {
         DocumentOperation.writeDocument(origPath,compPath,resultPath,similarity);
 
         System.out.println("耗时：" + (System.currentTimeMillis() - start) + " ms");
-    }}
+    }
+}
